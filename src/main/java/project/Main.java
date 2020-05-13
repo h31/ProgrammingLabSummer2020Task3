ae090fc655e7a6a6232332e07a9f48af7e62f239
Ilya <ilya.orlov2002@yandex.ru>
2020-05-13 22:36:36 +0400

    тестовый коммит

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("111");
         //Controller controller = new Controller();
-        //controller.launch();
+        //controller.launch()
         //System.out.println(controller.addWordToDict("кизлипричка"));
     }
 }
