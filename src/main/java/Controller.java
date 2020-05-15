c4da03a1e6ec9e338b3f10171f67a98a06a979bf
Artyom_Sidorov_3530901/90004 <artyom.sidoroff2014@yandex.ru>
2020-05-15 19:06:43 +0300

    "2048" version 1.0

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,11 +1,5 @@
 import com.sun.glass.ui.Robot;
-import com.sun.javafx.robot.FXRobot;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.AnchorPane;
-
 import java.awt.event.KeyEvent;
 
 public class Controller {
