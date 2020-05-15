aea1950780418a0c1cad0ba59938b3cbc7bdafd7
ilyaorlov25 <ilya.orlov2002@yandex.ru>
2020-05-15 15:49:53 +0400

    Немного методов со словами и добавил словарь 5-букв. слов

--- src/main/java/project/Controller.java
+++ src/main/java/project/Controller.java
@@ -6,23 +6,55 @@ import java.nio.file.Paths;
 
 public class Controller {
 
-    public void launch() {
+    public int getNumberOfLines(String filePath) {
+        int res = 0;
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String string = reader.readLine();
+            while (string != null) {
+                res++;
+                string = reader.readLine();
+            }
+            return res;
+        } catch (IOException e) {
+            return 0;
+        }
+    }
 
+    public String getFirstWord() throws IOException {
+        String filePath = "src/resources/five_let_words.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        int numOfLines = getNumberOfLines(filePath);
+        int random = (int)(( Math.random() * numOfLines + 1));
+        int i = 1;
+        String res;
+        try(reader) {
+            String string = reader.readLine();
+            while (i != random) {
+                string = reader.readLine();
+                i++;
+            }
+            res = string;
+        }
+        return res;
     }
 
     private boolean isWord(String checking) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("src/resources/dictionary.txt"));
-        String string = reader.readLine();
-        while (string != null) {
-            if (string.equals(checking)) return true;
-            string = reader.readLine();
+        try(reader) {
+            String string = reader.readLine();
+            while (string != null) {
+                if (string.equals(checking)) return true;
+                string = reader.readLine();
+            }
+            return false;
         }
-        return false;
     }
 
     public boolean addWordToDict(String word) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("src/resources/dictionary.txt"));
-        writer.write(word + "\n");
+        BufferedWriter writer = new BufferedWriter(new FileWriter("src/resources/dictionary.txt", true));
+        try (writer) {
+            writer.write("\n" + word);
+        }
         return isWord(word);
     }
 }
--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,12 +1,9 @@
 package project;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("111");
-        //Controller controller = new Controller();
-        //controller.launch()
-        //System.out.println(controller.addWordToDict("кизлипричка"));
+        Controller controller = new Controller();
     }
 }
