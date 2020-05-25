e83ddff90bd60c7a0bed79653d8c6b7bcb863424
ilyaorlov25 <ilya.orlov2002@yandex.ru>
2020-05-26 00:16:02 +0400

    Всяко разно 1.2

--- src/main/java/project/View.java
+++ src/main/java/project/View.java
@@ -1,6 +1,9 @@
 package project;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -27,8 +30,23 @@ public class View extends Application {
         controller.firstScoreText.setFont(Font.font(null, 20));
         controller.secondScoreText.setFont(Font.font(null, 20));
 
-        controller.restart.setLayoutX(950);
+        //размещение ещё надо настроить
+        controller.firstPlayerText.setLayoutX(600);
+        controller.firstPlayerText.setLayoutY(50);
+        controller.secondPlayerText.setLayoutX(750);
+        controller.secondPlayerText.setLayoutY(50);
+        controller.restart.setLayoutX(900);
         controller.restart.setLayoutY(100);
+        controller.skip.setLayoutX(900);
+        controller.skip.setLayoutY(200);
+
+        final Group root = new Group(controller.firstScoreText,
+                controller.secondScoreText, controller.firstPlayerText,
+                controller.secondPlayerText, controller.restart,
+                controller.skip);
+
+        final Scene scene = new Scene(root, Color.CORNFLOWERBLUE);
+        stage.setScene(scene);
 
         stage.show();
     }
